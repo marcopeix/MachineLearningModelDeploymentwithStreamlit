8a7ea5c5f4cf9e263d8c0d3fd292f08464c0f6c5
Marco Peixeiro <marcopeix@outlook.com>
2023-06-06 10:49:06 -0400

    Add starter code for capstone

---
+++ 12_dashboard_capstone/starter.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+URL = "https://raw.githubusercontent.com/marcopeix/streamlit-population-canada/master/data/quarterly_canada_population.csv"
+
+df = pd.read_csv(URL, dtype={'Quarter': str, 
+                            'Canada': np.int32,
+                            'Newfoundland and Labrador': np.int32,
+                            'Prince Edward Island': np.int32,
+                            'Nova Scotia': np.int32,
+                            'New Brunswick': np.int32,
+                            'Quebec': np.int32,
+                            'Ontario': np.int32,
+                            'Manitoba': np.int32,
+                            'Saskatchewan': np.int32,
+                            'Alberta': np.int32,
+                            'British Columbia': np.int32,
+                            'Yukon': np.int32,
+                            'Northwest Territories': np.int32,
+                            'Nunavut': np.int32})
+
+    
