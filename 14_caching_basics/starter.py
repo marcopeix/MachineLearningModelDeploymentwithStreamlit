9eb664a2dec4e232e34ff3af99f39c2db0154612
Marco Peixeiro <marcopeix@outlook.com>
2023-06-27 17:16:48 -0400

    Add starter file for lesson

---
+++ 14_caching_basics/starter.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import time
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+st.title("Caching demonstration")
+
+st.button('Test cache')
+
+st.subheader("st.cache_data")
+
+
+st.subheader("st.cache_resource")
+
+
