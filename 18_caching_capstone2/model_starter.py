1a4618a67711c7999ac9a18a119bb7f1e2b2f257
Marco Peixeiro <marcopeix@outlook.com>
2023-06-27 18:38:40 -0400

    Add comments

--- 18_caching_capstone2/model_starter.py
+++ 18_caching_capstone2/model_starter.py
@@ -3,3 +3,16 @@ from joblib import dump
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.ensemble import GradientBoostingClassifier
+
+URL = "https://raw.githubusercontent.com/marcopeix/MachineLearningModelDeploymentwithStreamlit/master/17_caching_capstone/data/mushrooms.csv"
+COLS = ['class', 'odor', 'gill-size', 'gill-color', 'stalk-surface-above-ring',
+       'stalk-surface-below-ring', 'stalk-color-above-ring',
+       'stalk-color-below-ring', 'ring-type', 'spore-print-color']
+
+# Read data
+
+# Create pipeline
+
+# Fit the pipeline
+
+# Save the pipeline
