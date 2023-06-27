ff7e6b578561d158d2ee70b626d88c07c99f135b
Marco Peixeiro <marcopeix@outlook.com>
2023-06-27 18:25:02 -0400

    Add starter files for capstone

---
+++ 18_caching_capstone2/model_starter.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from joblib import dump
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.ensemble import GradientBoostingClassifier
